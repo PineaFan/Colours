4b1e006900a0b7d018402120935eac881614c1cb
PineaFan <pineapplefanyt@gmail.com>
2023-12-22 22:58:53 +0000

    Waiting message is back

--- pages/[colour].tsx
+++ pages/[colour].tsx
@@ -2,10 +2,9 @@ import { useRouter } from 'next/router';
 import Colour from "../components/colour"
 
 export default function Page() {
-    let colour = "000000";
     const router = useRouter()
     // Check if the router has loaded or if the user just didn't select a pattern
-    if (!router.isReady) return </>
-    colour = (router.query.colour ? router.query.colour : "") as string
+    if (!router.isReady) return <>Waiting...</>
+    let colour = (router.query.colour ? router.query.colour : "") as string
     return <Colour currentColour={colour} />
 }
